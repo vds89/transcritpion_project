17497839bd1de85499859d363910c0562bcf6cf5
vds89 <vincenzodesimone04@gmail.com>
2023-10-23 20:28:39 +0200

    upload.html corrected

--- transcritpion_project/settings.py
+++ transcritpion_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hn=fyks#6u$@v8ya=lmqsr0!%*117+x845aw#*$mn+klw%_s25
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vds89.pythonanywhere.com']
+ALLOWED_HOSTS = ['vds89.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
